7ecd5e6ae2420e62fd915d1a0e2bf24867f18ac0
fdouled <102689016+fdouled@users.noreply.github.com>
2024-05-12 01:41:40 -0700

    mod-preview css

--- blooming/src/components/ModulePreview.js
+++ blooming/src/components/ModulePreview.js
@@ -3,6 +3,9 @@ import { NavBar } from "./NavBar";
 import { useState, useEffect } from "react";
 import { Link, useLocation } from 'react-router-dom';
 import next from '../img/chevron-right.svg';
+import "../components/modulePreview.css";
+
+
 
 export function ModulePreview(props) {
   const location = useLocation();
@@ -12,33 +15,31 @@ export function ModulePreview(props) {
     <div>
       <NavBar/>
       <div className="container">
-        <h1>{module.title}</h1>
-        <p>{module.desc}</p>
-        <p>This module contains {module.pages.length} parts.</p>
-        <p>
+        <h1 className="preview-title">{module.title}</h1>
+        <p className="mod-description">{module.desc}</p>
+        <p className="mod-contains">This module contains <span style={{color: '#7d8c65'}}>{module.pages.length}</span> parts.</p>
+        <p className="resource-intro">
           The information contained in this module comes from these websites:
-          <ul>
+          <ul style={{ listStyle: 'none' }}>
             {module.links.map((resource) => {
               return (
-                <li>
-                  <a href={resource.url}>{resource.name}</a>
+                <li className="resource-list">
+                <a href={resource.url}  className="resource-link">{resource.name}</a>
                 </li>
               )
             })}
           </ul>
           Visit these resources to learn more about this topic!
         </p>
-        <Link to={'/learn'}><button className="second-btn">Go Back</button></Link>
-        <Link to={'/module-lesson'} state={module}><button className="button">Start</button></Link>
-        <h2>Share Your Thoughts!</h2>
-        <p>Talk about your own experiences here! Let's build a community where we can become comfortable talking about these topics!</p>
-        <div class="card" style={{width: "18rem", backgroundColor: "#faf6ea", border: "1px solid #e9e2cf"}}>
-          <div class="card-body">
-            <h5 class="card-title">What’s a tip you have for dealing with period pain?</h5>
-            <p class="card-text">Write your answer here!</p>
-          </div>
-          <div class="card-body">
-            <a href="#" class="card-link">View Reflections<img className="next" src={next} alt=""></img></a>
+        <Link to={'/learn'}><button className="button-back">Go Back</button></Link>
+        <Link to={'/module-lesson'} state={module}><button className="button-preview">Start</button></Link>
+        <h2 className="share-thoughts">Share Your Thoughts!</h2>
+        <p className="lets-talk">Talk about your own experiences here! Let's build a community where we can become comfortable talking about these topics!</p>
+        <div class="reflection-card">
+            <h5 class="reflection-question">What's a tip you have for dealing with period pain?</h5>
+            <p class="write-answer">Write your answer here!</p>
+          <div class="reflection-card-body">
+            <a href="#" class="view-reflections">View Reflections<img className="next-arrow" src={next} alt=""></img></a>
           </div>
         </div>
       </div>
